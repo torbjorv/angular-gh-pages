ec06820916ca2d658ae0cb3640ba1333c380890c
CircleCI <torbjorv@gmail.com>
2019-08-09 13:32:49 -0700

    workign on updating the version list

--- versions/deploy.sh
+++ versions/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+# Inputs
+APP_VERSION=$1  #'prod' or ID
+APP_FOLDER=$2   # Copy the app from here
+URL_BASE=$3     # Complete base URL to gh-pages location (no trailing slash)
+CICD=$4         # URL for CI/CD
+SOURCE=$5       # URL for source
+
 deploy_to_folder()
 {
     local SRC=$1
@@ -18,52 +25,32 @@ deploy_to_folder()
     sed -i.bak 's|.*base href.*|<base href='"$BASE_HREF"'>|' $DEST/index.html
 }
 
-# Inputs
-APP_VERSION=$1 #'prod' or and ID
-APP_FOLDER=$2 
-URL_BASE=$3 # Include leading forward slash
+main() 
+{
+    if [ "$APP_VERSION" == "prod" ]; then
+        deploy_to_folder $APP_FOLDER . $URL_BASE/
+    else
+        deploy_to_folder $APP_FOLDER ./versions/$APP_VERSION $URL_BASE/versions/$APP_VERSION/
+        deploy_to_folder $APP_FOLDER ./versions/latest $URL_BASE/versions/latest/
 
-# Derived variables
-if [ "$APP_VERSION" == "prod" ]; then
-    # TARGET_FOLDER=.
-    # BASE_HREF=$URL_BASE/
-    deploy_to_folder $APP_FOLDER . $URL_BASE/
-else
-    deploy_to_folder $APP_FOLDER ./versions/$APP_VERSION $URL_BASE/versions/$APP_VERSION/
-    deploy_to_folder $APP_FOLDER ./versions/latest $URL_BASE/versions/latest/
-    # TARGET_FOLDER=./versions/$APP_VERSION
-    # BASE_HREF=$URL_BASE/versions/$APP_VERSION/
-fi
+        # Assume we're in a CircleCI context but could easily be parametrized
 
-# Setup and clean target folder. In case we update the root/prod/main version, make
-# sure we keep the 404.html and 'versions' folder
-# echo Deploying version $APP_VERSION
-# mkdir -p $TARGET_FOLDER
-# cd $TARGET_FOLDER  > /dev/null 2>&1
-# find . -name "*" -not \( -path "./.git*" -o -path "./versions*" -o -name 404.html \) -delete
-# cd -  > /dev/null 2>&1
+        DEPLOY_DATE=`date +%d%b%Y`
+        DEPLOY_URL=''
 
-# # Copy angular app
-# cp -a $APP_FOLDER/* $TARGET_FOLDER
-# sed -i.bak 's|.*base href.*|<base href='"$BASE_HREF"'>|' $TARGET_FOLDER/index.html
+        sed -i.bak '3i\
+        | '"$APP_VERSION"' | '"$DEPLOY_DATE"' | '"$URL_BASE/versions/$APP_VERSION"' | \
+        ' ./versions/versions.md
+    fi
 
-# if [ "$APP_VERSION" != "prod" ]; then
-#     echo Updating 'latest'
+    # echo Adding files to git...
+    # git add -A > /dev/null 2>&1
 
+    # echo Committing...
+    # # need 'ci skip' to ignore this branch in CircleCI
+    # git commit --allow-empty -m "Deploy to branch '$BRANCH' [ci skip]"  > /dev/null 2>&1
+    # git push --force --quiet origin $BRANCH > /dev/null 2>&1
+}
 
-#     mkdir -p $TARGET_FOLDER
-#     cd $TARGET_FOLDER  > /dev/null 2>&1
-#     find . -name "*" -not \( -path "./.git*" -o -path "./versions*" -o -name 404.html \) -delete
-#     cd -  > /dev/null 2>&1
-#     cp -a $APP_FOLDER/* ./versions/latest
-#     sed -i.bak 's|.*base href.*|<base href='"$BASE_HREF"'>|' $TARGET_FOLDER/index.html
+main
 
-# fi
-
-# echo Adding files to git...
-# git add -A > /dev/null 2>&1
-
-# echo Committing...
-# # need 'ci skip' to ignore this branch in CircleCI
-# git commit --allow-empty -m "Deploy to branch '$BRANCH' [ci skip]"  > /dev/null 2>&1
-# git push --force --quiet origin $BRANCH > /dev/null 2>&1
